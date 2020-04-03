ee06fc0df3632e387fbe7e5781132ca866384829
Justin Marshall <justin.marshall@shift-technology.com>
2020-04-03 15:01:40 +0200

    Retrieve information for all builds in the queue

--- TeamCityMonitoring/Monitoring/Monitor.cs
+++ TeamCityMonitoring/Monitoring/Monitor.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using TeamCityMonitoring.MonitoringMetrics;
 using TeamCityMonitoring.TeamCityService;
+using File = System.IO.File;
 
 namespace TeamCityMonitoring.Monitoring
 {
@@ -15,29 +22,55 @@ namespace TeamCityMonitoring.Monitoring
             httpClient.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", token);
             httpClient.BaseAddress = new Uri(url);
-            var client = new Client(httpClient) {BaseUrl = url};
+            var client = new Client(httpClient) { BaseUrl = url };
 
-            await LoopAsync(client, period, cancellationToken);
+            await LoopAsync(client, period, path, cancellationToken);
         }
 
-        private async Task LoopAsync(Client client, int period, CancellationToken cancellationToken)
+        private async Task LoopAsync(Client client, int period, string csvPath, CancellationToken cancellationToken)
         {
             Console.WriteLine("Beginning of monitoring");
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                ShouldQuote = (s, context) => true,
+                IgnoreBlankLines = true,
+                NewLine = NewLine.CRLF
+            };
 
             try
             {
                 var delay = TimeSpan.FromMinutes(period);
-                while (true)
+                using (var stream = File.OpenWrite(csvPath))
+                using (var writer = new StreamWriter(stream, Encoding.UTF8))
+                using (var csvWriter = new CsvWriter(writer, configuration))
                 {
-                    var result = await client.GetBuildsAsync(null, null, cancellationToken);
-                    Console.WriteLine($"Number of builds {result.Count} at {DateTime.Now}");
-                    await Task.Delay(delay, cancellationToken);
+                    csvWriter.WriteHeader<QueuedBuildStatus>();
+                    while (true)
+                    {
+                        var now = DateTime.UtcNow;
+                        var result = await client.GetBuildsAsync(null, null, cancellationToken);
+
+                        await Task.WhenAll(WriteRecordsAsync(result, csvWriter, writer, now), Task.Delay(delay, cancellationToken));
+                    }
                 }
             }
             catch (OperationCanceledException)
             {
                 Console.WriteLine("End of monitoring");
             }
+        }
+
+        private async Task WriteRecordsAsync(Builds result, IWriter csvWriter, TextWriter writer, DateTime now)
+        {
+            Console.WriteLine($"Number of builds {result.Count} at {now}");
+            foreach (var build in result.Build)
+            {
+                csvWriter.WriteRecord(new QueuedBuildStatus { NumberOfBuilds = result.Count ?? 0, Timestamp = now, Id = build.Id?.ToString(), Branch = build.BranchName, Type = build.BuildTypeId });
+                csvWriter.NextRecord();
+            }
+            await csvWriter.FlushAsync();
+            await writer.FlushAsync();
         }
     }
 }
---
+++ TeamCityMonitoring/MonitoringMetrics/QueueBuildNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace TeamCityMonitoring.MonitoringMetrics
+{
+    public class QueueBuildNumber
+    {
+        [Name("Number of builds")]
+        public int NumberOfBuilds { get; set; }
+        [Name("Timestamp UTC")]
+        public DateTime Timestamp { get; set; }
+    }
+
+    public class QueuedBuildStatus
+    {
+        [Name("Number of builds")]
+        public int NumberOfBuilds { get; set; }
+        [Name("Timestamp UTC")]
+        public DateTime Timestamp { get; set; }
+        [Name("Build Id")]
+        public string Id { get; set; }
+        [Name("Build Type Id")]
+        public string Type { get; set; }
+        [Name("Branch")]
+        public string Branch { get; set; }
+
+    }
+}
